f94a4e7fa65195532892873a9879c381fafa8ee0
Alison Wu <alison.wu23@gmail.com>
2023-05-12 18:04:34 +0800

    chore: install @heroicons/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
 function App() {
   return (
-    <div className='bg-blue-500 text-white underline'>Hello, Tailwind CSS!</div>
+    <>
+      <div className='bg-blue-500 text-white underline'>Hello, Tailwind CSS!</div>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
+    </>
   )
 }
 
